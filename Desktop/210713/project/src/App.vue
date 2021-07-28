00f459a4e93139ca2ba949de0a4acfa6d5e4d0b2
hililyy <hililyy>
2021-07-28 10:50:23 +0900

    210728

--- Desktop/210713/project/src/App.vue
+++ Desktop/210713/project/src/App.vue
@@ -1,48 +1,16 @@
 <template>
-    <item-add-remove-form
-      :max-height = "200"
-      :data = "data"
-      :form = "form"
-      title = "파일 설정"
-    />
-
-<!--
-      @add-disable = "add_disable"
-      @remove-disable = "remove_disable"
-      @change = "change"
-      :classes = "Object"
-    -->
-
+  <div>
+    <setting-component></setting-component>
+  </div>
 </template>
 
 <script>
-import ItemAddRemoveForm from '@/components/ItemAddRemoveForm.vue'
+import SettingComponent from '@/components/SettingComponent.vue'
+
 
 export default  { 
-  data(){
-    return {
-        data: [],
-        form : [{
-            // form 1)
-            field : 'key',  // 우리가 사용하는 부분
-            type : 'autocomplete',  //text, autocomplete만
-            label : 'key',  // placeholder
-            option : [], //autocomplete에서 사용하는 option-
-        //  //   width : 400, 
-        },
-        { // form 2)
-            field : 'value',
-            type : 'text',
-            label : 'value',
-        }
-      ],
-    }
-  },
-  methods : {
-    
-  },
-  components : {
-    ItemAddRemoveForm
+  components:{
+    SettingComponent
   }
 }
 </script>
---
+++ Desktop/210713/project/src/components/EssentialInfoSetting.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class = "Setting">
+        <ul class = "menu">
+            <label>필수 정보</label>
+        </ul>
+        <div class = "element">
+            <ul class = "ElementOption" v-for = "(option,index) in Option" v-bind:key="index">
+                <li>{{option}}</li>
+                <input type="text">
+            </ul>
+        </div>        
+    </div>
+</template>
+
+<script>
+export default {
+   data(){
+       return{
+           Option : ["IP 주소","호스트 이름","장비 이름","모델 이름"],
+       }
+    }
+}
+</script>
+<style scoped>
+    .element{
+        padding : 15px 0px 0px 10px;
+        border:1px solid grey;
+        border-top-width : 0px;
+        height : 120px;
+        width : 500px;
+        flex-wrap: wrap;
+        
+    }
+    .ElementOption{
+        display : inline-block;
+    }
+    .ElementOption li {
+        margin : 10px 0px 10px 20px;
+        text-align: left;
+        list-style: none;
+        display : inline;
+        float : left;
+        width : 80px;
+        
+    }
+    .ElementOption input{
+        border:1px solid grey;
+        margin : 5px 10px 5px 0px;
+        width : 110px;
+        height : 30px;
+        float : right;
+    }
+    .menu {
+        padding : 10px 0px 10px 10px;
+        margin-bottom : 0; 
+        border:1px solid grey;
+        height : 40px;
+    }
+    .menu li { 
+        padding-left:0px;
+        display : inline;
+        list-style:none;
+    }
+    .Setting { 
+    width : 500px;
+    margin : 20px 20px 40px 20px;
+  }
+</style>
